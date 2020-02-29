de5ea0af9d6d9edc20cf81d997c77aebcedadafd
Piar1986 <polienkoivan86@gmail.com>
2020-02-29 18:19:49 +0700

    Correction errors steps 23-27

--- scripts.py
+++ scripts.py
@@ -7,7 +7,13 @@ from datacenter.models import Commendation
 from django.core.exceptions import ObjectDoesNotExist
 from django.core.exceptions import MultipleObjectsReturned
 
-child = Schoolkid.objects.get(full_name__contains='Фролов Иван')
+child_name = 'Фролов Иван'
+try:
+    child = Schoolkid.objects.get(full_name__contains=child_name)
+except ObjectDoesNotExist:
+    print("Ученик не найден")
+except MultipleObjectsReturned:
+    print("Найдено несколько учеников. Конкретизируйте запрос.")
 
 def fix_marks(schoolkid):
 	bad_points=[2,3]
@@ -18,16 +24,14 @@ def fix_marks(schoolkid):
 		mark.save()
 
 def remove_chastisements(schoolkid):
-    child_chastisements=Chastisement.objects.filter(schoolkid=schoolkid)
-    for chastisement in child_chastisements:
-        child_chastisement=Chastisement.objects.get(id=chastisement.id)
-        child_chastisement.delete()
+    chastisements=Chastisement.objects.filter(schoolkid=schoolkid)
+    for chastisement in chastisements:
+        Chastisement.objects.get(id=chastisement.id).delete()        
 
 Commendations=['Молодец!','Отлично!', 'Хорошо!', 'Гораздо лучше, чем я ожидал!', 'Ты меня приятно удивил!', 'Великолепно!', 'Прекрасно!', 'Ты меня очень обрадовал!', 'Именно этого я давно ждал от тебя!', 'Сказано здорово – просто и ясно!', 'Ты, как всегда, точен!', 'Очень хороший ответ!', 'Талантливо!', 'Ты сегодня прыгнул выше головы!', 'Я поражен!', 'Уже существенно лучше!', 'Потрясающе!', 'Замечательно!', 'Прекрасное начало!', 'Так держать!', 'Ты на верном пути!', 'Здорово!', 'Это как раз то, что нужно!', 'Я тобой горжусь!', 'С каждым разом у тебя получается всё лучше!', 'Мы с тобой не зря поработали!', 'Я вижу, как ты стараешься!', 'Ты растешь над собой!', 'Ты многое сделал, я это вижу!', 'Теперь у тебя точно все получится!']
 
-def create_commendation(child_name, subject_title):
+def create_commendation(child, subject_title):
     try:
-        child = Schoolkid.objects.get(full_name__contains=child_name)
         lesson=Lesson.objects.filter(subject__title=subject_title, year_of_study=child.year_of_study, group_letter=child.group_letter)[0]
         Commendation.objects.create(text=random.choice(Commendations), created=lesson.date, schoolkid=child, subject=lesson.subject, teacher=lesson.teacher)
     except ObjectDoesNotExist:
