45aa2216785728009fb63b439545c7c2867d1b70
IvanPolienko <piar1986@yandex.ru>
2020-03-01 12:46:24 +0700

    replaced tab with spaces, fixed remove_chastisements

--- scripts.py
+++ scripts.py
@@ -9,32 +9,33 @@ from django.core.exceptions import MultipleObjectsReturned
 
 child_name = 'Фролов Иван'
 try:
-	child = Schoolkid.objects.get(full_name__contains=child_name)
+    child = Schoolkid.objects.get(full_name__contains=child_name)
 except ObjectDoesNotExist:
-	print("Ученик не найден")
+    print("Ученик не найден")
 except MultipleObjectsReturned:
-	print("Найдено несколько учеников. Конкретизируйте запрос.")
+    print("Найдено несколько учеников. Конкретизируйте запрос.")
 
 def fix_marks(schoolkid):
-	bad_points=[2,3]
-	child_points=Mark.objects.filter(schoolkid=schoolkid, points__in=bad_points)
-	for point in child_points:
-		mark = Mark.objects.get(id=point.id)
-		mark.points = 5
-		mark.save()
+    bad_points=[2,3]
+    child_points=Mark.objects.filter(schoolkid=schoolkid, points__in=bad_points)
+    for point in child_points:
+        mark = Mark.objects.get(id=point.id)
+        mark.points = 5
+        mark.save()
 
 def remove_chastisements(schoolkid):
-	chastisements=Chastisement.objects.filter(schoolkid=schoolkid)
-	for chastisement in chastisements:
-		Chastisement.objects.get(id=chastisement.id).delete()        
+    chastisements=Chastisement.objects.filter(schoolkid=schoolkid)
+    for chastisement in chastisements:
+    	chastisement.delete()
+        #Chastisement.objects.get(id=chastisement.id).delete()      
 
 Commendations=['Молодец!','Отлично!', 'Хорошо!', 'Гораздо лучше, чем я ожидал!', 'Ты меня приятно удивил!', 'Великолепно!', 'Прекрасно!', 'Ты меня очень обрадовал!', 'Именно этого я давно ждал от тебя!', 'Сказано здорово – просто и ясно!', 'Ты, как всегда, точен!', 'Очень хороший ответ!', 'Талантливо!', 'Ты сегодня прыгнул выше головы!', 'Я поражен!', 'Уже существенно лучше!', 'Потрясающе!', 'Замечательно!', 'Прекрасное начало!', 'Так держать!', 'Ты на верном пути!', 'Здорово!', 'Это как раз то, что нужно!', 'Я тобой горжусь!', 'С каждым разом у тебя получается всё лучше!', 'Мы с тобой не зря поработали!', 'Я вижу, как ты стараешься!', 'Ты растешь над собой!', 'Ты многое сделал, я это вижу!', 'Теперь у тебя точно все получится!']
 
 def create_commendation(schoolkid, subject_title):
-	try:
-		lesson=Lesson.objects.get(id=(Lesson.objects.filter(subject__title=subject_title, year_of_study=schoolkid.year_of_study, group_letter=schoolkid.group_letter).order_by('-date').first()).id)
-		Commendation.objects.create(text=random.choice(Commendations), created=lesson.date, schoolkid=schoolkid, subject=lesson.subject, teacher=lesson.teacher)
-	except ObjectDoesNotExist:
-		print("Не найден ученик или предмет")
-	except MultipleObjectsReturned:
-		print("Найдено несколько результатов. Конкретизируйте запрос.")
+    try:
+        lesson=Lesson.objects.get(id=(Lesson.objects.filter(subject__title=subject_title, year_of_study=schoolkid.year_of_study, group_letter=schoolkid.group_letter).order_by('-date').first()).id)
+        Commendation.objects.create(text=random.choice(Commendations), created=lesson.date, schoolkid=schoolkid, subject=lesson.subject, teacher=lesson.teacher)
+    except ObjectDoesNotExist:
+        print("Не найден ученик или предмет")
+    except MultipleObjectsReturned:
+        print("Найдено несколько результатов. Конкретизируйте запрос.")
